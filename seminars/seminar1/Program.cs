26e64258c8e8d51e7c1ca1bee894a2dfcf408149
Mikhail Kamishov <pi_ff@mail.ru>
2023-02-10 21:24:15 +0300

    First commit

---
+++ seminars/seminar1/Program.cs
@@ -0,0 +1,71 @@
+﻿// Console.WriteLine("Input number");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int quad = num * num;
+// Console.WriteLine($"Quad of {num} is {quad}");
+// Задача 2
+
+// Console.WriteLine("Input first number");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input second number");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// int quad = num2 * num2;
+
+// if (quad == num1)
+// {
+//     Console.WriteLine("First number is quad of second number");
+
+// }
+// else 
+// {
+//     Console.WriteLine("First number is not quad of second number");
+
+// }
+
+
+
+
+
+// Задача 3. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> " -2, -1, 0, 1, 2"
+
+// Console.WriteLine("Input number");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int current = num * (-1);
+// while(current <= num)
+// {
+//     Console.Write(current + " ");
+//     current++;
+// }
+
+
+
+
+// Console.WriteLine("Input number");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int current = num * (-1);
+// while(current <= num)
+// {
+//      Console.Write(num + " ");
+//      num--;
+//  }
+
+// //1. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает **последнюю** цифру этого числа.
+    
+//    // 456 -> 6
+    
+//     // 782 -> 2
+    
+//     // 918 -> 8
+
+Console.WriteLine("Input three-digit number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result = num % 10;
+
+Console.WriteLine($"Last digit of {num} is {result}");
+
