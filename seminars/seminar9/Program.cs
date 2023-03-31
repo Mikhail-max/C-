32b864f7f30950fbd64c318243467edb6a67da6c
Mikhail Kamishov <pi_ff@mail.ru>
2023-03-31 10:13:27 +0300

    itog rabota

--- seminars/seminar9/Program.cs
+++ seminars/seminar9/Program.cs
@@ -21,27 +21,44 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
-int Sum (int m, int n)
-{
-    int summa = 0;
-    if (n != m)
-    {
-        if (m > n)
-        {
-            return Sum( (m - 1) + (n + 1));
-        }
-        else
-        {
-            return Sum( (m + 1) + (n - 1));
-        }
-    } 
-    else
-    {
-        return Sum (m + n);
-    }
-}
-Console.WriteLine("Input M");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Input N");
-int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(Sum(m, n));
+// Console.Write("Введите значение M: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите значение N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int summa(int m, int n)
+// {
+// if (m==n)
+// return n;
+// else
+// {
+//     if (m < n)
+//     {
+//         return m + summa(m + 1, n);
+//     }
+//     else
+//     {
+//         return n + summa(m, n + 1);
+//     }
+// }
+// }
+// Console.Write($"Сумма натуральных элементов в промежутке от M до N равно {summa(m,n)}");
+
+// // Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// // m = 2, n = 3 -> A(m,n) = 9
+// // m = 3, n = 2 -> A(m,n) = 29
+
+// int Akkerman (int m, int n)
+// {
+//   if (m == 0)
+//     return n + 1;
+//   else
+//     if ((m != 0) && (n == 0))
+//       return Akkerman(m - 1, 1);
+//     else
+//       return Akkerman(m - 1, Akkerman(m, n - 1));
+// }
+// Console.Write("Введите значение M: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите значение N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write($"Функция Аккермана при числе M({m}) и N({n}) равна {Akkerman(m,n)}");
